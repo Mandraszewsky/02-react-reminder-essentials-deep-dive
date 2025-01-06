c933e4538219c06e55f15289860550c59ef035ac
Mandraszewsky <mandrah@o2.pl>
2025-01-06 16:27:25 +0100

    updateting object (array) state immutably with multi-dimensional lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player  name="Player 1" symbol="X"/>
-          <Player  name="Player 2" symbol="O"/>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
